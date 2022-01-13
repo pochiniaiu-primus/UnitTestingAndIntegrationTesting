6d236e973136379cd6f7601ae1740f8ce36a19ef
Serhii Honchar <sehonc@softserveinc.com>
2022-01-13 17:03:33 +0200

    configure app

---
+++ src/main/java/com/serhiihonchar/unitandintegrationtesting/Gender.java
@@ -0,0 +1,7 @@
+package com.serhiihonchar.unitandintegrationtesting;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/test/java/com/serhiihonchar/unitandintegrationtesting/UnitAndIntegrationTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhiihonchar.unitandintegrationtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitAndIntegrationTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
