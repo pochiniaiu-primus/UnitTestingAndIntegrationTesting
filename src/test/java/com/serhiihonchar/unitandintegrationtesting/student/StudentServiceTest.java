c5c38023b12d5757cac70b52a8cc1283c0bc4850
Serhii Honchar <sehonc@softserveinc.com>
2022-01-13 23:16:37 +0200

    testing getAllStudents() and addStudent(Student student) from StudentService using Mockito

---
+++ src/test/java/com/serhiihonchar/unitandintegrationtesting/student/StudentServiceTest.java
@@ -0,0 +1,56 @@
+package com.serhiihonchar.unitandintegrationtesting.student;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceTest {
+    @Mock
+    private StudentRepository studentRepository;
+    private StudentService underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new StudentService(studentRepository);
+    }
+
+    @Test
+    void canGetAllStudents() {
+        //when
+        underTest.getAllStudents();
+        //then
+        verify(studentRepository).findAll();
+
+    }
+
+    @Test
+    void canAddStudent() {
+        //given
+        String email = "serhii@honchar.com";
+        Student student = new Student("Serhii", email, Gender.MALE);
+        //when
+        underTest.addStudent(student);
+        //then
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+
+        Student capturedStudent = studentArgumentCaptor.getValue();
+
+        assertThat(capturedStudent).isEqualTo(student);
+
+    }
+
+    @Test
+    @Disabled
+    void deleteStudent() {
+    }
+}
